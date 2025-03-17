b0ef825cc0012434f30e848f92dde5a4fb47a900
heliagln <145973164+heliagln@users.noreply.github.com>
2025-03-17 11:43:16 +0000

    add decision_tree pratically finished

--- decision_tree.py
+++ decision_tree.py
@@ -36,11 +36,29 @@ def draw_samples(model, X_test, sensitive_columns, non_sensitive_columns, num_sa
   prediction = pd.DataFrame(model.predict(np.array(df_random)), index=df_random.index, columns=['prediction'])
   return df_random, prediction
 
+def analyze_threshold(sample_a, borne, feature_name, minmax, threshold):
+    series = pd.Series({
+        'sample' : sample_a.copy(),
+        'feature_name' : feature_name,
+        'first_threshold' : threshold,
+        'second_threshold' : borne.loc[feature_name, minmax],
+    })
+    borne.loc[feature_name, minmax] = threshold
+    return series
+    # sample_b = sample_a.copy()
+    # sample_b[feature_name] = random.uniform(threshold, borne.loc[feature_name, minmax])
+    # predict_sample_b = (model.predict((np.array(sample_b)).reshape(1, -1)))[0][0]
+    
+    '''
+    if abs(predict_sample - predict_sample_b) >= 0.05:
+        return 1, [(sample_a, sample_b)]
+    else:
+        return 0, []'''
+    
+
 # random path
 def random_path(model, tree, X_test, sensitive_columns):
     sample = X_test.sample(n=1) # X_test.iloc[np.random.choice(len(X_test))]
-
-    predict_sample = (model.predict((np.array(sample)).reshape(1, -1)))[0][0]
 
     decision_path = tree.decision_path(sample)
 
@@ -50,76 +68,88 @@ def random_path(model, tree, X_test, sensitive_columns):
         'max' : [X_test[col].max() for col in sensitive_columns],
         'min' : [X_test[col].min() for col in sensitive_columns]
         }, index=sensitive_columns)
-
+    
     sample_a = sample.iloc[0]
+    columns = X_test.columns
+    sensible_nodes = [(columns[tree.tree_.feature[node]], tree.tree_.threshold[node]) for node in node_indices if columns[tree.tree_.feature[node]] in sensitive_columns]
+    df_path = pd.DataFrame(columns=['sample', 'feature_name', 'first_threshold', 'second_threshold'])
 
-    tryout = 0
-    disc_number = 0
-    disc_sample = []
-
+    if sensible_nodes != []:
+        for feature_name, threshold in sensible_nodes:
+            if sample_a[feature_name] <= threshold:
+                series = analyze_threshold(sample_a, borne, feature_name, 'max', threshold)
+                df_path = pd.concat([df_path, series.to_frame().T], ignore_index= True)
+            else:
+                series = analyze_threshold(sample_a, borne, feature_name, 'min', threshold)
+                df_path = pd.concat([df_path, series.to_frame().T], ignore_index= True)
+    return df_path          
+    '''
     for node in node_indices:
-        columns = X_test.columns
         feature_name = columns[tree.tree_.feature[node]]
         threshold = tree.tree_.threshold[node]
         if feature_name in sensitive_columns:
             if sample_a[feature_name] <= threshold:
-                sample_b = sample_a.copy()
-                sample_b[feature_name] = random.uniform(threshold, borne.loc[feature_name, 'max'])
-                predict_sample_b = (model.predict((np.array(sample_b)).reshape(1, -1)))[0][0]
-                if abs(predict_sample - predict_sample_b) >= 0.05:
-                    disc_number += 1
-                    disc_sample.append((sample_a, sample_b))
-                    print('yes')
-                borne.loc[feature_name, 'max'] = threshold
+                if_disc, sample_disc = analyze_threshold(model, sample_a, predict_sample, borne, feature_name, 'max', threshold)
+                disc_number += if_disc
+                disc_sample = disc_sample + sample_disc
             else:
-                sample_b = sample_a.copy()
-                sample_b[feature_name] = random.uniform(borne.loc[feature_name, 'min'], threshold)
-                predict_sample_b = (model.predict((np.array(sample_b)).reshape(1, -1)))[0][0]
-                if abs(predict_sample - predict_sample_b) >= 0.05:
-                    disc_number += 1
-                    disc_sample.append((sample_a, sample_b))
-                    print('yes')
-                borne.loc[feature_name, 'min'] = threshold
-            tryout += 1
-    return tryout, disc_number, disc_sample
+                if_disc, sample_disc = analyze_threshold(model, sample_a, predict_sample, borne, feature_name, 'min', threshold)
+                disc_number += if_disc
+                disc_sample = disc_sample + sample_disc
+            tryout += 1'''
+    
+def collect_sample(row):
+    return row['sample'].copy()
 
-
-# evaluate discrimination knowing the difference in predictions
-def evaluate_discrimination(pred_diff, boundary_samples, modified_samples, threshold = 0.05):
-  # check for discrimination
-  mask = (pred_diff > threshold)
-  first_half = mask[:(int(len(mask)/2))]
-  # print(sum(first_half))
-  second_half = mask[(int(len(mask)/2)):]
-  # print(sum(second_half))
-  indices = np.where(mask)[0]
-  number_discrimination = len(indices)
-  discrimination_samples = [(boundary_samples.loc[i], modified_samples.loc[i]) for i in indices]
-  return number_discrimination, discrimination_samples
+def modify_sample(row):
+    sample = row['sample'].copy()
+    col = row['feature_name']
+    first_threshold, second_threshold = row['first_threshold'], row['second_threshold']
+    new_value = random.uniform(first_threshold, second_threshold)
+    sample[col] = new_value
+    return sample
 
 
 # Calculate the IDI ratio
-def calculate_idi_ratio_tool(model, X_test, sensitive_columns, non_sensitive_columns, num_samples=1000, num_seed = 100):
+def calculate_idi_ratio_tool(model, X_test, sensitive_columns, non_sensitive_columns, num_samples=1000, num_seed = 100, threshold=0.05):
     # Draw sample
-    df_random, prediction = draw_samples(model, X_test, sensitive_columns, non_sensitive_columns, num_samples)
+    df_random, prediction = draw_samples(model, X_test, sensitive_columns, non_sensitive_columns, 500)
     prediction_class = np.where((np.array(prediction))>=0.5, 1, 0)
 
     # decision tree training
     tree = DecisionTreeClassifier()
     tree.fit(df_random, prediction_class)
 
+    # extract paths
+    df_path = pd.DataFrame(columns=['sample', 'feature_name', 'first_threshold', 'second_threshold', 'prediction'])
+
+    while len(df_path) < num_seed:
+        df_result = random_path(model, tree, X_test, sensitive_columns)
+        df_path = pd.concat([df_path, df_result], ignore_index=True)
+    
+    df_path = df_path.head(num_seed)
+
+    # prediction
+    array_for_prediction = np.stack(df_path['sample'].values)
+    prediction = model.predict(array_for_prediction)
+
     # needed information
+    samples = df_path.apply(collect_sample, axis=1)
     try_number = 0
-    disc = 0
+    number_discrimination = 0
     discrimination_pairs = []
 
-    for i in range(num_samples):
-        tryout, disc_number, disc_sample = random_path(model, tree, X_test, sensitive_columns)
-        try_number += tryout
-        disc += disc_number
-        discrimination_pairs = discrimination_pairs + disc_sample
+    for i in range(int(num_samples/num_seed)):
+        df_modifed_samples = df_path.apply(modify_sample, axis=1)
+        modified_prediction = model.predict(np.array(df_modifed_samples))
+        pred_diff = abs(prediction - modified_prediction)
+        mask = (pred_diff > threshold)
+        indices = np.where(mask)[0]
+        number_discrimination += len(indices)
+        discrimination_pairs = discrimination_pairs + [(samples.iloc[i], df_modifed_samples.iloc[i]) for i in indices]
+        try_number += len(df_modifed_samples)
     
-    return disc, try_number # discrimination_number / num_samples, discrimination_samples
+    return number_discrimination, try_number # discrimination_number / num_samples, discrimination_samples
 
 # 6. Main function
 def main():
@@ -135,7 +165,7 @@ def main():
     non_sensitive_columns = [col for col in X_test.columns if col not in sensitive_columns]
 
     # 3. Calculate and print the Individual Discrimination Instance Ratio
-    disc, tryout = calculate_idi_ratio_tool(model, X_test, sensitive_columns, non_sensitive_columns, num_samples=500, num_seed=50)
+    disc, tryout = calculate_idi_ratio_tool(model, X_test, sensitive_columns, non_sensitive_columns, num_samples=500, num_seed=100)
     print(f"number of tryout : {tryout} and number for discrimination : {disc}")
     print(f"IDI Ratio: {disc/tryout}")
 
